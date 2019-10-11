0eed19e8c94ea2c773aba3dbd8e47797092a07a0
Muhamed Didovic <muhamed.didovic@gmail.com>
2019-10-11 16:07:17 +0200

    update routes to comply with package design

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,8 +1,12 @@
 <?php
 
-Route::post('short', 'LinkController@store');
-Route::get('short', 'LinkController@show');
-Route::get('stats', 'LinkStatsController@show');
+Route::group(['namespace' => 'MuhamedDidovic\Shortener\Controllers'], function()
+{
+    Route::post('short', 'LinkController@store');
+    Route::get('short', 'LinkController@show');
+    Route::get('stats', 'LinkStatsController@show');
 
-//ROUTE for vue
-Route::get('/{any?}', 'SinglePageController@show')->where('any', '.*');
+    //ROUTE for vue
+    Route::get('/{any?}', 'SinglePageController@show')->where('any', '.*');
+});
+
