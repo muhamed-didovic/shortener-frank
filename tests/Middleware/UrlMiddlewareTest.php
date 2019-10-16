ab550bf2ec41e7bc69e953f406733380b0df6d8c
Muhamed Didovic <muhamed.didovic@gmail.com>
2019-10-16 17:47:37 +0200

    add middleware test

---
+++ tests/Middleware/UrlMiddlewareTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Tests\Middleware;
+
+use Illuminate\Http\Request;
+use MuhamedDidovic\Shortener\Test\TestCase;
+
+class UrlMiddlewareTest extends TestCase
+{
+    /** @test */
+    public function http_is_prepended_to_url()
+    {
+        $request = new Request;
+
+        $request->replace([
+            'url' => 'google.com'
+        ]);
+
+        $middleware = new \MuhamedDidovic\Shortener\Middleware\ModifiesUrlRequestData;
+
+        $middleware->handle($request, function ($req) {
+            $this->assertEquals('http://google.com', $req->url);
+        });
+    }
+
+    /** @test */
+    public function http_is_not_prepended_to_url_if_scheme_exists()
+    {
+        $request = new Request;
+
+        $urls = [
+            'ftp://www.google.com',
+            'http://www.google.com',
+            'https://www.google.com',
+        ];
+
+        foreach ($urls as $url) {
+            $request->replace([
+                'url' => $url
+            ]);
+
+            $middleware = new \MuhamedDidovic\Shortener\Middleware\ModifiesUrlRequestData;
+
+            $middleware->handle($request, function ($req) use ($url) {
+                $this->assertEquals($url, $req->url);
+            });
+        }
+    }
+}
