62e66ffa63a7e6857842f115677c0392dd9fc2d8
Muhamed Didovic <muhamed.didovic@gmail.com>
2019-10-18 15:44:48 +0200

    add comments and explanations for config file

--- config/shortener.php
+++ config/shortener.php
@@ -1,12 +1,37 @@
 <?php
 return [
-
+    /**
+     * Name of table where links should be stored
+     */
     'table'      => 'links',
+
+    /**
+     * Url that should be used with shortened string
+     */
     'url'        => env('APP_URL', 'http://frank.test'),
+
+    /**
+     * Routes used in package
+     */
     'routes'     => [
+        /**
+         * Route used to store url
+         */
         'post_short_route' => 'short',
+
+        /**
+         * Rotued to get shortend url
+         */
         'get_short_route'  => 'short',
+
+        /**
+         * Route to get status of url provided
+         */
         'get_stats_route'  => 'stats',
+
+        /**
+         * Route to serve Vue instance 
+         */
         'vue_route'        => '{any?}',
     ],
 ];
