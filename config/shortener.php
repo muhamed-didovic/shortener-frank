ba5409317d92fc936dfb4a0098e8c29eb8568d75
Muhamed Didovic <muhamed.didovic@gmail.com>
2019-10-18 15:02:29 +0200

    make routes dynamic

--- config/shortener.php
+++ config/shortener.php
@@ -1,6 +1,12 @@
 <?php
 return [
-    
+
     'table' => 'links',
     'url'   => env('APP_URL', 'http://frank.test'),
+    'routes' => [
+        'post_short_route' => '/short',
+        'get_short_route' => '/short',
+        'get_stats_route' => '/stats',
+        'vue_route' => '{any?}'
+    ]
 ];
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,12 +1,19 @@
 <?php
 
-Route::group(['namespace' => 'MuhamedDidovic\Shortener\Controllers', 'middleware' => 'MuhamedDidovic\Shortener\Middleware\ModifiesUrlRequestData'], function()
-{
-    Route::post('short', 'LinkController@store');
-    Route::get('short', 'LinkController@show');
-    Route::get('stats', 'LinkStatsController@show');
-
-    //ROUTE for vue
-    Route::get('/{any?}', 'SinglePageController@show')->where('any', '.*');
-});
+Route::group(
+    [
+        'namespace'  => 'MuhamedDidovic\Shortener\Controllers',
+        'middleware' => 'MuhamedDidovic\Shortener\Middleware\ModifiesUrlRequestData',
+    ],
+    function () {
+        //save url
+        Route::post(config('shortener.routes.post_short_route'), 'LinkController@store');
+        //get url
+        Route::get(config('shortener.routes.get_short_route'), 'LinkController@show');
+        //get stats
+        Route::get(config('shortener.routes.get_stats_route'), 'LinkStatsController@show');
+        //ROUTE for vue
+        Route::get(config('shortener.routes.vue_route'), 'SinglePageController@show')->where('any', '.*');
+    }
+);
 
