ac33b486bb1d9c224b62581286e3cb18a37c71df
Muhamed Didovic <muhamed.didovic@gmail.com>
2019-10-25 16:26:32 +0200

    update files

--- src/ShortenerServiceProvider.php
+++ src/ShortenerServiceProvider.php
@@ -13,28 +13,53 @@ use Laravel\Lumen\Application as LumenApplication;
 class ShortenerServiceProvider extends ServiceProvider
 {
     /**
-     * Bootstrap services.
+     * Bootstrap the application services.
      *
      * @return void
      */
     public function boot()
     {
+        /**
+         * Add observer for Link model
+         */
         Link::observe(LinkObserver::class);
 
-        $source = realpath($raw = __DIR__ . '/../config/shortener.php') ?: $raw;
         //        if ($this->app instanceof LaravelApplication && $this->app->runningInConsole()) {
-
+        /*
+         * Optional methods to load your package assets
+         */
+        $source = realpath($raw = __DIR__ . '/../config/shortener.php') ?: $raw;
         $this->loadViewsFrom(__DIR__ . '/views', 'shortener');
         $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
         $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
 
         $this->publishes([
-            $source                         => config_path('shortener.php'),
-            __DIR__ . '/../database/migrations'         => database_path('migrations'),
+            $source => config_path('shortener.php'),
+        ], 'config');
+
+        $this->publishes([
+            __DIR__ . '/../database/migrations' => database_path('migrations'),
+        ], 'migrations');
+
+        // Publishing the views.
+        $this->publishes([
             __DIR__ . '/../resources/views' => resource_path('views/vendor/shortener'),
-            __DIR__ . '/../public/css'      => public_path('css/'),
-            __DIR__ . '/../public/js'       => public_path('js/'),
-        ], 'shortener');
+        ], 'views');
+
+        // Publishing assets.
+        $this->publishes([
+            __DIR__ . '/../resources/assets' => public_path('vendor/test'),
+            __DIR__ . '/../public/css'       => public_path('css/'),
+            __DIR__ . '/../public/js'        => public_path('js/'),
+        ], 'assets');
+
+        // Publishing the translation files.
+        /*$this->publishes([
+            __DIR__.'/../resources/lang' => resource_path('lang/vendor/test'),
+        ], 'lang');*/
+
+        // Registering package commands.
+        // $this->commands([]);
 
         //        $this->publishes([
         //            __DIR__ . '/views' => resource_path('views/vendor/shortener'),
@@ -49,13 +74,15 @@ class ShortenerServiceProvider extends ServiceProvider
     }
 
     /**
-     * Register services.
+     * Register the application services.
      *
      * @return void
      */
     public function register()
     {
         $source = realpath($raw = __DIR__ . '/../config/shortener.php') ?: $raw;
+
+        // Automatically apply the package configuration
         $this->mergeConfigFrom($source, 'shortener');
     }
 }
